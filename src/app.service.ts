b850d67102da7523394053cb3f788851223a7f83
Guilherme Tabuada <ggtbh@outlook.com>
2023-05-23 15:56:02 -0300

    feat: prisma init

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  getHello(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }
