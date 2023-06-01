1c4e36fa6c9352ff650fde20c51b6a192fcbffaf
William Fernandes <williamjosefernandes@gmail.com>
2023-06-01 16:09:41 -0300

    Ajustando retorno das apis

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,3 +1,3 @@
-import { CreateUserDto } from './create-user.dto';
+import { ResponseUserDto } from './user.dto';
 
-export type UpdateUserDto = Partial<CreateUserDto>;
+export type UpdateUserDto = Partial<ResponseUserDto>;
