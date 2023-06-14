9b3839cc756c66eac2ec935089e3cdba53dc0d40
William Fernandes <williamjosefernandes@gmail.com>
2023-06-14 14:42:17 -0300

    Ajuste do seed com nova senha

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,7 +19,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Andria Bluschke',
         document: '8917404804',
         email: 'abluschke0@hc360.com',
@@ -30,7 +31,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Stormie Munson',
         document: '2220389421',
         email: 'smunson1@blinklist.com',
@@ -41,7 +43,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Ford Leavry',
         document: '7843663711',
         email: 'fleavry2@ameblo.jp',
@@ -52,7 +55,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Roseline Cornish',
         document: '7449994739',
         email: 'rcornish3@telegraph.co.uk',
@@ -63,7 +67,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Hamel Horder',
         document: '3140339690',
         email: 'hhorder4@discuz.net',
@@ -74,7 +79,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Jonah Vice',
         document: '9424612346',
         email: 'jvice5@wufoo.com',
@@ -85,7 +91,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Normy Benn',
         document: '1199955876',
         email: 'nbenn6@squarespace.com',
@@ -96,7 +103,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Laney Calcraft',
         document: '6637235598',
         email: 'lcalcraft7@theatlantic.com',
@@ -107,7 +115,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Cobbie Yuryichev',
         document: '7581428397',
         email: 'cyuryichev8@nationalgeographic.com',
@@ -118,7 +127,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Miof mela Armfirld',
         document: '2259795951',
         email: 'mmela9@ustream.tv',
@@ -129,7 +139,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Griswold Dencs',
         document: '6958859316',
         email: 'gdencs0@uol.com.br',
@@ -140,7 +151,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Christabella Hammond',
         document: '8340577719',
         email: 'chammond1@photobucket.com',
@@ -151,7 +163,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Carina Larvor',
         document: '9717350140',
         email: 'clarvor2@icio.us',
@@ -162,7 +175,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Gayler Mallall',
         document: '9540670519',
         email: 'gmallall3@dot.gov',
@@ -173,7 +187,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Northrup Manueau',
         document: '1880980703',
         email: 'nmanueau4@prweb.com',
@@ -184,7 +199,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Guglielmo Birkby',
         document: '3183903792',
         email: 'gbirkby5@usa.gov',
@@ -195,7 +211,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Mallory Andri',
         document: '9167455743',
         email: 'mandri6@bloglovin.com',
@@ -206,7 +223,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Georgia Freke',
         document: '5918706615',
         email: 'gfreke7@un.org',
@@ -217,7 +235,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Corette Aneley',
         document: '1800836104',
         email: 'caneley8@accuweather.com',
@@ -228,7 +247,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Petrina Martineau',
         document: '4516996006',
         email: 'pmartineau9@mapquest.com',
@@ -239,7 +259,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Chancey Matei',
         document: '2460000731',
         email: 'cmateia@bing.com',
@@ -250,7 +271,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Yasmin Panton',
         document: '7611027735',
         email: 'ypantonb@hud.gov',
@@ -261,7 +283,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Kirbie Stockley',
         document: '1470800969',
         email: 'kstockleyc@forbes.com',
@@ -272,7 +295,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Elinor Cud',
         document: '1699452660',
         email: 'ecudd@arizona.edu',
@@ -283,7 +307,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Keene Alan',
         document: '0043684165',
         email: 'kalane@cdbaby.com',
@@ -294,7 +319,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Jaynell Dunniom',
         document: '6905764779',
         email: 'jdunniomf@dedecms.com',
@@ -305,7 +331,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Nataniel Teasdale-Markie',
         document: '4771982325',
         email: 'nteasdalemarkieg@jugem.jp',
@@ -316,7 +343,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Kasey Blackaller',
         document: '2675748382',
         email: 'kblackallerh@mashable.com',
@@ -327,7 +355,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: 'Randal Walne',
         document: '1907519696',
         email: 'rwalnei@virginia.edu',
@@ -338,7 +367,8 @@ async function main() {
       {
         id: uuidv4(),
         status: 'ACTIVE',
-        password: 'edu312',
+        password:
+          '$2b$10$eMCiyozJb3ChrOm55B4lt.3bIpx5GjXEQFtCeWT0166INp/PsdDT6',
         name: "Ossie O'Brien",
         document: '9214318255',
         email: 'oobrienj@lycos.com',
