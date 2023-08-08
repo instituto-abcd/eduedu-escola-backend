049d4efb9ad1a74ea02663395ce6dea6b44e9cf3
Éder Franco <eder.f@lyncas.net>
2023-08-08 14:56:54 -0300

    Adicionando Firebase Service Account como Argumento de Build

--- src/planet-sync/firebase.service.ts
+++ src/planet-sync/firebase.service.ts
@@ -1,8 +1,7 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './service-account.json';
 
 export const firebaseApp = admin.initializeApp({
   projectId: 'eduedu-escola-hub---stg',
   storageBucket: 'eduedu-escola-hub---stg.appspot.com',
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(JSON.parse(process.env.FIRESTORE_READ_SERVICEACCOUNT) as admin.ServiceAccount),
 });
