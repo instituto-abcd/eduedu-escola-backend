160905d4ef85357670bfff6e6b0f2c29301ade6c
Éder Franco <eder.f@lyncas.net>
2023-10-04 16:26:31 -0300

    Retornando lista estática de modelos completa no endpoint de listagem para debug

--- src/planet/planet.service.ts
+++ src/planet/planet.service.ts
@@ -24,21 +24,68 @@ export class PlanetService {
   }
 
   async findPlanetModels() : Promise<any> {
-    const planets = await this.planetModel.find();
-    const questions = planets.reduce((question, planet) => [ ...question, ...planet.questions ], []);
+    // const planets = await this.planetModel.find();
+    // const questions = planets.reduce((question, planet) => [ ...question, ...planet.questions ], []);
 
-    let models = questions.map((question) => {
-      return question.model_id
-    });
+    // let models = questions.map((question) => {
+    //   return question.model_id
+    // });
 
-    let examModels = await this.findExamModels();
-    models.push(...examModels);
+    let models = [
+      "MODEL2",
+      "MODEL3",
+      "MODEL4",
+      "MODEL5",
+      "MODEL8",
+      "MODEL10",
+      "MODEL11",
+      "MODEL12",
+      "MODEL13",
+      "MODEL14",
+      "MODEL15",
+      "MODEL16",
+      "MODEL18",
+      "MODEL19",
+      "MODEL20",
+      "MODEL21",
+      "MODEL22",
+      "MODEL24",
+      "MODEL25",
+      "MODEL26",
+      "MODEL27",
+      "MODEL28",
+      "MODEL29",
+      "MODEL30",
+      "MODEL31",
+      "MODEL32",
+      "MODEL33",
+      "MODEL34",
+      "MODEL35",
+      "MODEL10-PROVA",
+      "MODEL11-PROVA",
+      "MODEL18-PROVA",
+      "MODEL2-VIDEO",
+      "MODEL8-PROVA",
+      "QME2x2Audio",
+      "QME2x2Text",
+      "QME2x2Video",
+      "QMES2x3Video",
+      "QMES5",
+      "QORD3x2",
+    ];
 
-    const uniqueModels = models.filter((n, i) => models.indexOf(n) === i);
-
-    return uniqueModels.sort(
+    return models.sort(
       (a, b) => a.localeCompare(b),
     );
+
+    // let examModels = await this.findExamModels();
+    // models.push(...examModels);
+
+    // const uniqueModels = models.filter((n, i) => models.indexOf(n) === i);
+
+    // return uniqueModels.sort(
+    //   (a, b) => a.localeCompare(b),
+    // );
   }
 
   private async findExamModels(): Promise<any> {
