b634ac0d17f5b8082e34a1b3263492b9052de18f
gtabuada <ggtbh@outlook.com>
2025-06-16 12:00:05 -0300

    Request error logger

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { SchoolClassScheduleService } from './schedules/school-class-schedule.service';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { WinstonModule } from "nest-winston";
+import { AppModule } from "./app.module";
+import { instance } from "./logger/logger.module";
+import { SchoolClassScheduleService } from "./schedules/school-class-schedule.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		logger: WinstonModule.createLogger({
+			instance,
+		}),
+	});
 
-  const config = new DocumentBuilder()
-    .setTitle('EduEdu Escola')
-    .setDescription('EduEdu Escola API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+	const config = new DocumentBuilder()
+		.setTitle("EduEdu Escola")
+		.setDescription("EduEdu Escola API")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("swagger", app, document);
 
-  app.enableCors();
+	app.enableCors();
 
-  const schoolClassService = app.get(SchoolClassScheduleService);
-  await schoolClassService.updateSchoolClassStudentsStartApp();
+	const schoolClassService = app.get(SchoolClassScheduleService);
+	await schoolClassService.updateSchoolClassStudentsStartApp();
 
-  await app.listen(3000);
+	await app.listen(3000);
 }
 bootstrap();
